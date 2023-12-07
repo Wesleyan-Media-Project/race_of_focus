be0f89ad7059aa90247cf0b59105725e51693cd2
furkmak <93638016+furkmak@users.noreply.github.com>
2023-12-06 19:18:49 -0500

    Add files via upload

--- race_of_focus_google_2022.R
+++ race_of_focus_google_2022.R
@@ -11,16 +11,16 @@ library(tidyverse)
 
 # Option to use only the text-based fields to decide race of focus
 # Set to False by default
-textonly <- F
-#textonly <- T
+#textonly <- F
+textonly <- T
 
 ####Input Files####
 #goggle files
-path_g2022_vars <- "../google_2022/google2022_adidlevel.csv"
+path_g2022_vars <- "../google_2022/google2022_adidlevel_merged.csv"
 g2022 <- fread(path_g2022_vars, encoding = "UTF-8")
 
 #entity linking files
-path_el_results <- "../entity_linking_2022/google/data/entity_linking_results_google_2022_notext_combined_20231028.csv.gz"
+path_el_results <- "../entity_linking_2022/google/data/entity_linking_results_google_2022_notext_combined.csv.gz"
 entity <- fread(path_el_results, encoding = "UTF-8")
 
 path_wmpent <- "../datasets/wmp_entity_files/Google/wmp_google_2022_entities_v112822.csv"
@@ -114,15 +114,25 @@ if(textonly == T){
   g2022_3 <- g2022_2 %>% select(-c(detected_entities, aws_face))
 }
 
-#Exclusions (need a new list)####
+#Exclusions
 # exclude scotus, former potus candidates, senators not up for reelection
-#pol <- fread(path_cand_pol)
-#df$all_entities <- 
-#  lapply(df$all_entities, function(x){x[!x %in% pol$fec_ids]}) %>%
+pol <- fread(path_cand_pol)
+
+not_relevant_person <- pol[,which(names(pol) == "supcourt_2022"):which(names(pol) == "gov2022_noelect")]
+not_relevant_person[is.na(not_relevant_person) == T] <- 0
+# also include cabinet members etc. who don't have their own variable
+not_relevant_person$other <- as.numeric(pol$face_category != "")
+# anyone who has at least one 1 is not relevant
+not_relevant_person <- apply(not_relevant_person, 1, function(x){any(x == 1)})
+not_relevant_person <- pol$wmpid[not_relevant_person]
+
+g2022_3$all_entities <- 
+  lapply(g2022_3$all_entities, function(x){x[!x %in% not_relevant_person]}) %>%
   # if one of those people is the only one mentioned, 
   # it will be set to character(0), changing this to ""
-#  lapply(function(x){x[x != ""]})
-#rm(pol)
+  lapply(function(x){x[x != ""]})
+rm(pol)
+g2022_3$all_entities[unlist(lapply(g2022_3$all_entities, length)) == 0] <- ""
 
 
 #####
@@ -196,6 +206,58 @@ check3 <- lapply(check3, function(x){x[is.na(x) == F]})
 #g2022_3$matched_mentions_region_pct <- map2(g2022_3$region_pct, check3, function(x,y){x[y]})
 
 
+
+####
+# Note that the results are only reliable for ads that are actually in bucket 3.2.2.x
+
+# Extract regional impressions (state & percentage)
+# Start extracting the state from region distribution
+g2022_3$region_distribution <- 
+  g2022_3$region_distribution %>%
+  str_replace_all("Washington, District of Columbia", "Washington District of Columbia") %>% 
+  str_remove_all('\\"') %>% 
+  str_remove_all('percentage:') %>% 
+  str_remove_all(',region') %>% 
+  str_remove_all('\\[|\\]|\\{|\\}')
+
+# Split regions
+tmp <- str_split(g2022_3$region_distribution, ",")
+# For each region, split the percentage and the region name
+tmp <- lapply(tmp, str_split_fixed, ":", n = 2)
+# Only keep regions that are states
+# the ,,drop = F avoids converting one-row matrices into vectors
+# the first comma is for the subsetting, the second to specify arguments
+tmp <- lapply(tmp, function(x){x[x[,2] %in% state.name,,drop = F]})
+# Put each column of the resulting into the respective column in the dataframe
+g2022_3$region_pct <- lapply(tmp, function(x){x[,1]})
+g2022_3$region_distribution <- lapply(tmp, function(x){x[,2]})
+# Convert state names to state abbreviations
+g2022_3$region_distribution <- lapply(g2022_3$region_distribution, function(x){state.abb[match(x, state.name)]})
+
+# States of mentioned entities (without presidential candidates)
+g2022_3$entities_states <- 
+  lapply(g2022_3$all_unique_entities_unique_races_no_pres, substr, 1, 2) %>%
+  lapply(unique)
+
+# For each state in the region distribution
+# check whether it is contained in the states of the mentioned entities
+# And what it's position in that vector is
+check <- map2(g2022_3$region_distribution, g2022_3$entities_states, function(x,y){match(x, y)})
+# Only keep the non-NAs
+check2 <- lapply(check, function(x){x[is.na(x) == F]})
+# Races with overlap
+g2022_3$matched_mentions <- map2(g2022_3$all_unique_entities_unique_races_no_pres, check2, function(x,y){x[y]})
+# Set NAs to character(0), otherwise the next step sometimes works incorrectly
+g2022_3$matched_mentions[is.na(g2022_3$matched_mentions)] <- list(as.character())
+# How many of these are there?
+g2022_3$state_matches <- unlist(lapply(check2, length))
+# Add an indicator to the df for whether the ad has a match
+g2022_3$state_match_binary <- ifelse(g2022_3$state_matches == 0, 0, 1)
+# Impressions proportion of those races
+check3 <- map2(g2022_3$entities_states, g2022_3$region_distribution, function(x,y){match(x, y)})
+check3 <- lapply(check3, function(x){x[is.na(x) == F]})
+g2022_3$matched_mentions_region_pct <- map2(g2022_3$region_pct, check3, function(x,y){x[y]})
+####
 # ----
 # Buckets
 
